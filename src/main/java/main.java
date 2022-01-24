34ec87a4083fae1491be1c1539d37b74e720749c
evy <coevyat3@gmail.com>
2022-01-24 19:22:38 +0200

    vol

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,7 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("hello");
+       for(int i=0;i<5;i++){
+           System.out.println("hello "+i);
+       }
     }
 }
